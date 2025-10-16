b5e13db864a06ebc281cc64f3d35628f184c101e
AutismKing <antalyk007@gmail.com>
2025-10-16 15:31:08 +0100

    Add files via upload

---
+++ new_script_for_gamejam.gd
@@ -0,0 +1,19 @@
+extends Node
+func main_menu():
+	Sprite2D
+func _on_start_pressed() -> void:
+	print("Start pressed") # Replace with function body.
+func _on_options_pressed() -> void:
+	print("Options pressed")
+func _on_exit_pressed() -> void:
+	print("Exit pressed")
+func viewport():
+	var viewport = get_viewport()
+var mousepos = get_viewport().get_mouse_position()
+ #if mousepos.x > PLACEHOLDER and < PLACEHOLDER:
+#inputevent
+
+func ai_for_characters():
+	var ai_character_1 = 0
+
+	
